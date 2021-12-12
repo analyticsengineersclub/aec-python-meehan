166d2cd0c8ca3a7f58c23ec6875479897c0ef48e
datadisciple <sean@sparkgrills.com>
2021-12-12 22:15:15 +0000

    adds test_subtract & refactors relevant calc.py code

--- calc.py
+++ calc.py
@@ -15,32 +15,42 @@ add.add_argument("ints_to_sum", nargs='*', type=int)
 sub = subparsers.add_parser("sub", help = "subtract integers")
 sub.add_argument("ints_to_subtract", nargs=2, type=int)
 
+def aec_subtract(ints_to_subtract):
+    our_sub = ints_to_subtract[0] - ints_to_subtract[1]
+    print(f"the difference between values is: {our_sub}")
+    return(our_sub)
+
 multiply = subparsers.add_parser("multiply", help = "multiply integers")
 multiply.add_argument("ints_to_multiply", nargs='+', type=int)
 
 divide = subparsers.add_parser("divide", help = "divide integers")
 divide.add_argument("ints_to_divide", nargs=2, type=int)
 
-args = parser.parse_args()
+# Nesting everyythin under this ensures that this code isn't run during tests
+# & instead only runs when called on from the CLI
+# Without this, an error would be thrown when tests are run because it would be looking for an argument (ie. add, sub, multiply, divide)
+if __name__ == "__main__":
+    args = parser.parse_args()
 
-# Print command output
-if args.command == "add":
-    our_sum = sum(args.ints_to_sum)
-    print(f"the sum of values is: {our_sum}")
+    # Print command output
+    if args.command == "add":
+        our_sum = sum(args.ints_to_sum)
+        print(f"the sum of values is: {our_sum}")
 
-if args.command == "sub":
-    our_sub = args.ints_to_subtract[0] - args.ints_to_subtract[1]
-    print(f"the difference between values is: {our_sub}")
+    if args.command == "sub":
+        # our_sub = args.ints_to_subtract[0] - args.ints_to_subtract[1]
+        # print(f"the difference between values is: {our_sub}")
+        aec_subtract(args.ints_to_subtract)
 
-if args.command == "multiply":
-    our_product = args.ints_to_multiply[0]
-    for i in args.ints_to_multiply[1:]:
-        our_product *= i
-    print(f"the product of the values is: {our_product}")
+    if args.command == "multiply":
+        our_product = args.ints_to_multiply[0]
+        for i in args.ints_to_multiply[1:]:
+            our_product *= i
+        print(f"the product of the values is: {our_product}")
 
-if args.command == "divide":
-    if args.ints_to_divide[1] == 0:
-        print("Cannot divide by 0")
-    else:
-        our_quotient = args.ints_to_divide[0] / args.ints_to_divide[1]
-        print(f"the quotient of values is: {our_quotient}")
+    if args.command == "divide":
+        if args.ints_to_divide[1] == 0:
+            print("Cannot divide by 0")
+        else:
+            our_quotient = args.ints_to_divide[0] / args.ints_to_divide[1]
+            print(f"the quotient of values is: {our_quotient}")
