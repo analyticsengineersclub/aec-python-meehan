ab9c0280bd67701f3efa0925a293aed5e97899cb
datadisciple <sean@sparkgrills.com>
2021-12-13 00:12:17 +0000

    adding addtl notes to assertRaises
    also shows a secondary formatting method

--- tests/test_too_many_args.py
+++ tests/test_too_many_args.py
@@ -5,5 +5,11 @@ from calc import aec_subtract, aec_divide
 class TestArgs(unittest.TestCase):
 
     def test_limit_two_args(self):
+        # Asserts that an exception is thrown if specified values are passed in
+        # If no exception is failed, this assertion will not be true, and the test will fail        
         self.assertRaises(Exception, aec_subtract, [6, 3, 2])
-        self.assertRaises(Exception, aec_divide, [20, 2, 5, 4])
+
+        with self.assertRaises(Exception):
+            aec_divide([20, 2, 5, 4])
+        
+        # ^ this is just another way to format a similar assertion
